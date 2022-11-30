7da4c346844c3d5a8d6e071f6f6bb1ebbedf4c1b
i.a.tsyman <mr.tsyman@mail.ru>
2022-11-30 18:04:29 +0400

    [+] Red: тест обновления поля

--- Assets/Tests/EditMode/GameFieldModelTest.cs
+++ Assets/Tests/EditMode/GameFieldModelTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using FluentAssert;
 using GameCore.Models;
@@ -27,6 +28,36 @@ namespace Tests.EditMode
 
             // assert
             gameFieldModel.Cards.Where(c => c != null).Count().ShouldBeEqualTo(totalCards);
+        }
+        
+        [Test]
+        public void WhenGameFieldInitializedRefreshed_ThenAllCardsImageUpdated()
+        {
+            // prepare
+            var gameObject = new GameObject();
+            GameFieldModel gameFieldModel = gameObject.AddComponent<GameFieldModel>();
+            int totalCards = 6;
+            var imageLoader = Substitute.For<ILoadImage>();
+            imageLoader.LoadRandomTexture(Arg.Any<int>(), Arg.Any<int>())
+                .Returns(new Texture2D(200, 200));
+            gameFieldModel.ConstructFromCode(totalCards, imageLoader);
+            gameFieldModel.Start();
+
+            var cardsID = gameFieldModel.Cards.Select(c => c.GetInstanceID()).ToList();
+
+            // act
+            gameFieldModel.Refresh();
+
+            var refreshedIds = gameFieldModel.Cards.Select(c => c.GetInstanceID()).ToList();
+            // assert
+            List<bool> notRefreshed = new List<bool>();
+            
+            for (int i = 0; i < cardsID.Count(); i++)
+            {
+                notRefreshed.Add(cardsID[i] == refreshedIds[i]);    
+            }
+
+            notRefreshed.Count(result => result).ShouldBeEqualTo(0);
         }
     }
 }
