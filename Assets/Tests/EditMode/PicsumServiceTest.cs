28e39a1c5c3f791656f96d50f943eb4b7140c41f
i.a.tsyman <mr.tsyman@mail.ru>
2022-11-30 16:02:18 +0400

    [+] Red: тест загрузки картинки

---
+++ Assets/Tests/EditMode/PicsumServiceTest.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using FluentAssert;
+using ImageLoading;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityHttpWrapper;
+
+namespace Tests.EditMode
+{
+    public class PicsumServiceTest
+    {
+        [Test]
+        public async Task WhenLoadRandomTextureAndHttpWorksFine_ThenReturnNotNullAndNotEmptyTexture()
+        {
+            // prepare
+            var gameObject = new GameObject();
+            var service = gameObject.AddComponent<PicsumService>();
+            var httpClient = Substitute.For<IHttpClient>();
+            var mockImage = MockImage;
+            var data = mockImage.EncodeToJPG();
+            httpClient.SendRequest(Arg.Any<string>()).Returns(data);
+            service.Resolve(httpClient);
+
+            // act
+            Task<Texture2D> request = service.LoadRandomTexture(200, 200);
+            await request;
+
+            // assert
+            request.Result.ShouldNotBeNull();
+            request.Result.width.ShouldBeEqualTo(mockImage.width);
+            request.Result.height.ShouldBeEqualTo(mockImage.height);
+        }
+
+        private static Texture2D MockImage => AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Tests/SampleData/1.jpg");
+    }
+}
